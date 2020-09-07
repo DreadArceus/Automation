eb5986999823bbfe73c16afd08efb0bd94ea9ddd
Mayank Talwar <dcmtalwar@gmail.com>
2020-09-07 16:00:54 +0530

    Logger on hold

--- Logger/main.cpp
+++ Logger/main.cpp
@@ -1,14 +1,23 @@
+//Left for later, file system interactions are complicated.
 #include <bits/stdc++.h>
 #include <sys/stat.h>
 #include <unistd.h>
 using namespace std;
 int main()
 {
-    chdir("/Volumes/Macintosh HD - Data/Logs/");
-    ofstream o("August/test.txt");
-    if(!o.is_open())
+    const char* baseDir = "/Volumes/Macintosh HD - Data/Logs";
+        chdir(baseDir);
+    struct stat buf;
+    if( stat( baseDir, &buf) != 0 )
+    {
+        cout << "Unable to access the path";
+        //return -1;
+    }
+    //if( S_ISDIR(buf.st_mode) )
     {
         mkdir("August", 0777);
     }
+
+    ofstream o("test.txt");
     o << time(0);
 }
