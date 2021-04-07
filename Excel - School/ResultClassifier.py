c44882b9beae4e57e45194940353a380e78c3e32
dreadarceus <dreadarceus@gmail.com>
2021-04-07 19:23:14 +0530

    v2

--- Excel - School/ResultClassifier.py
+++ Excel - School/ResultClassifier.py
@@ -1,7 +1,10 @@
 from openpyxl import load_workbook
 from openpyxl import Workbook
 
-wb = load_workbook(filename=str(input()))
+opt = int(input("Enter 1 for 80 scaling, 2 for 50 scaling: "))
+name = str(input("Enter filepath (filename if in same folder): "))
+
+wb = load_workbook(filename=name)
 ws = wb.worksheets[0].title
 ws = wb[ws]
 wr = Workbook()
@@ -9,15 +12,15 @@ result = wr.create_sheet(title="Result")
 
 subjects = {}
 starting_column = "D"
-while ws[starting_column + '5'].value:
-    subjects[ws[starting_column + '5'].value] = {"column": starting_column, "distribution": [0 for _ in range(10)],
+while ws[starting_column + '4'].value:
+    subjects[ws[starting_column + '4'].value] = {"column": starting_column, "distribution": [0 for _ in range(8 if opt == 1 else 5)],
                                                  "passing": 0, "failing": 0}
     starting_column = chr(ord(starting_column) + 1)
 
-for i in range(10):
+for i in range(8 if opt == 1 else 5):
     result[f'{chr(ord("A") + i + 1)}' + '1'] = f'{10 * i} to {10 * (i + 1)}'
-result[f'{chr(ord("A") + 11)}' + "1"] = "Pas"
-result[f'{chr(ord("A") + 12)}' + "1"] = "Fail"
+result[f'{chr(ord("A") + (8 if opt == 1 else 5) + 1)}' + "1"] = "Pass"
+result[f'{chr(ord("A") + (8 if opt == 1 else 5) + 2)}' + "1"] = "Fail"
 
 for key, value in subjects.items():
     sr = 9
@@ -25,7 +28,14 @@ for key, value in subjects.items():
         max_marks = int(str(ws[value["column"] + f'{sr - 1}'].value)[1:])
     except ValueError:
         continue
-    scale = 100 / max_marks
+    passing_marks = 0
+    scale = 1
+    if opt == 1:
+        passing_marks = 28
+        scale = 80 / max_marks
+    elif opt == 2:
+        passing_marks = 17.5
+        scale = 50 / max_marks
     while ws[value["column"] + f'{sr}'].value or ws[value["column"] + f'{sr}'].value == 0:
         try:
             marks = float(ws[value["column"] + f'{sr}'].value) * scale
@@ -34,17 +44,17 @@ for key, value in subjects.items():
                 sr += 2
                 continue
             marks = 0
-        if marks >= 33:
+        if marks >= passing_marks:
             value["passing"] += 1
         else:
             value["failing"] += 1
-        value["distribution"][min(int(marks / 10), 9)] += 1
+        value["distribution"][min(int(marks / 10), int(max_marks * scale / 10 - 1))] += 1
         sr += 2
 
     result["A" + f'{ord(value["column"]) - ord("A")}'] = key
     for i in range(len(value["distribution"])):
         result[f'{chr(ord("A") + 1 + i)}' + f'{ord(value["column"]) - ord("A")}'] = value["distribution"][i]
-    result[f'{chr(ord("A") + 11)}' + f'{ord(value["column"]) - ord("A")}'] = value["passing"]
-    result[f'{chr(ord("A") + 12)}' + f'{ord(value["column"]) - ord("A")}'] = value["failing"]
+    result[f'{chr(ord("A") + (8 if opt == 1 else 5) + 1)}' + f'{ord(value["column"]) - ord("A")}'] = value["passing"]
+    result[f'{chr(ord("A") + (8 if opt == 1 else 5) + 2)}' + f'{ord(value["column"]) - ord("A")}'] = value["failing"]
 
-wr.save(filename='result.xlsx')
+wr.save(filename=f'output_{name}')
